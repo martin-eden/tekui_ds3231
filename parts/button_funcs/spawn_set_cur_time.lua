4151e0ce5cb839be6b391c9b04f11a275c5c41cc
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2022-04-03 14:14:21 -0400

    [.^] [spawn_set_cur_time]

--- parts/button_funcs/spawn_set_cur_time.lua
+++ parts/button_funcs/spawn_set_cur_time.lua
@@ -1,20 +1,26 @@
 local merge = request('!.table.merge')
 
-local to_rtc_time =
-  function(lua_time)
-    local dow = (lua_time.wday + 6) % 7
+local create_moment_record =
+  function(cur_time)
+    local lua_time_tbl = os.date('*t', cur_time)
+
+    -- <.wday> == 1 is Sunday. We want 1 to be Monday.\
+    local dow = lua_time_tbl.wday
+    assert(dow >= 1)
+    dow = dow - 1
     if (dow == 0) then
       dow = 7
     end
+
     return
       {
-        year = lua_time.year,
-        month = lua_time.month,
+        year = lua_time_tbl.year,
+        month = lua_time_tbl.month,
         dow = dow,
-        date = lua_time.day,
-        hour = lua_time.hour,
-        minute = lua_time.min,
-        second = lua_time.sec,
+        date = lua_time_tbl.day,
+        hour = lua_time_tbl.hour,
+        minute = lua_time_tbl.min,
+        second = lua_time_tbl.sec,
       }
   end
 
@@ -22,8 +28,10 @@ local set_cur_moment =
   function(rtc_rec)
     local round_trip_time_sec = 2
     local cur_time = os.time() + round_trip_time_sec
-    local cur_time_table = os.date('*t', cur_time)
-    merge(rtc_rec.moment, to_rtc_time(cur_time_table))
+
+    local moment = create_moment_record(cur_time)
+
+    merge(rtc_rec.moment, moment)
     rtc_rec.at_battery.clock_was_stopped = false
   end
 
